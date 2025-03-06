d44abdc7572fb716970371e5cf033260b7912761
luoleixi <2841817200@qq.com>
2025-03-06 23:02:56 +0800

    2025_3.6 commit

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,24 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node* p = head->next;
+    head->value = 1;
+    int st = 0;
+    while (p->next) {
+        if (p->value) {
+            st = 1;
+            break;
+        }
+        p->value = 1;
+        p = p->next;
+    }
+    p = head->next;
+    while (p->value) {
+        p->value = 0;
+        p = p->next;
+    }
+    if(st){
+    	return 1;
+	}
     return 0;
 }
