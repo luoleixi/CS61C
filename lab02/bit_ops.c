5f155c77192a6a1a57c4b4894233f4550e791d4a
luoleixi <2841817200@qq.com>
2025-03-08 22:59:40 +0800

    bit_ops

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -4,24 +4,43 @@
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns
-    // 0 or 1)
+    unsigned n) {
+    int cnt = x;
+    cnt = cnt >> n;
+    if (cnt & 1) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
     return -1;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
+void set_bit(unsigned* x,
+    unsigned n,
+    unsigned v) {
     // YOUR CODE HERE
+    unsigned cnt = *x;
+    cnt = cnt >> n;
+    unsigned tmp = *x - (cnt << n);
+    if (v)cnt = cnt | 1;
+    else cnt = cnt & ((~0) - 1);
+    cnt = cnt << n;
+    *x = cnt + tmp;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
+void flip_bit(unsigned* x,
+    unsigned n) {
     // YOUR CODE HERE
+    unsigned cnt = *x;
+    cnt = cnt >> n;
+    unsigned tmp = *x - (cnt << n);
+    unsigned flag = cnt & 1;
+    if (flag) cnt = cnt & ((~0) - 1);
+    else cnt = cnt | 1;
+    cnt = cnt << n;
+    *x = cnt + tmp;
 }
 
