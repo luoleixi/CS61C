1a5b2f6b9440539845ab0bb9f2809d5b084b6eb4
luoleixi <2841817200@qq.com>
2025-03-10 23:48:16 +0800

    lfsr

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,9 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t regs =	*reg; 
+	uint16_t msb = ((regs&0x1)^((regs>>2)&0x1)^((regs>>3)&0x1)^((regs>>5)&0x1))<<15;
+	(*reg) >>= 1;
+	(*reg) |= msb;
 }
 
